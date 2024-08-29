3a2abb6535efd07e8cee87896f2356a786482974
sauloads15 <saulosmithads15@gmail.com>
2024-08-29 12:24:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// const numeros= [1,2,3,4,5];
+
+// const multiplicarPorDois= numeros.map(numero => numero * 2);
+// const calcularQuadrado = numeros.map(numero => numero ** 2);
+
+// console.log("Array orginal:", numeros);
+// console.log("Cada numero multiplicado por dois:", multiplicarPorDois);
+// console.log("Quadrado de cada numero:", calcularQuadrado);
+
+// const arr= ['a','b','c','d','e','f']
+// function tornarMaiuscula(elemento, indice, array){
+//     return elemento.toUpperCase()
+// }
+// const newArr= arr.map(tornarMaiuscula)
+
+// const newArr2= arr.map(elemento=> elemento.toUpperCase())
+// const ages= [32,33,12,40]
+// function checkAge(age){
+//     return age > document.getElementById("ageToCheck").ariaValueMax;
+// }
+// function myFunction(){
+//     document.getElementById("demo").innerHTML= ages.filter(checkAge);
+// }
+// questao 1
+
+// const array= [25,24,22,57,53];
+// const dobroArray= array.map(numero => numero * 2);
+// console.log(dobroArray);
+
+//questao 2
+// const array = [1,2,4,5,7,8,9,10,12,13];
+// const paresArray = array.filter(numero => numero % 2 === 0);
+// console.log(paresArray);
+
+//questao 3:
+// const numeros= [1,2,3,4,5,6];
+// const soma= numeros.reduce((acumulador, numero) => acumulador + numero,0);
+// console.log("Array original:", numeros);
+// console.log("Soma dos numeros:",soma);
+
+//questao 4:
+// const numeros= [10,20,30,40];
+// const calcularQuadrado = numeros.map(numero => numero ** 2);
+// console.log("Array orginal:", numeros);
+// console.log("Quadrado de cada numero:", calcularQuadrado);
+
